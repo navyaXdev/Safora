381887c50a51ab9c392c04aa1922da42c86b5029
navyaXdev <navya.dinesh25@gmail.com>
2026-07-05 17:31:34 +0530

    config file fix

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,5 +1,5 @@
 
-const API_URL = `${import.meta.env.VITE_BACKEND_URL}/predict` ;
+const API_URL = `https://0.0.0.0/predict` ;
 
 function getRiskTier(riskScore){
     if (riskScore >= 0.7) return 'high';
